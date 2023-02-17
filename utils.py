f8be03ef357d033aa4604889383cacb6795d6730
bboygg <bboygg94@gmail.com>
2023-02-17 16:51:43 +0900

    Implement put timestamp code, Now you will have to do the following:
    1) get the correct file path from the uploads
    2) set the target file to write timestamp, and save
    3) Implent this code into app.py, so make it less complex as mvp
    
    Keep going

--- utils.py
+++ utils.py
@@ -2,51 +2,69 @@ import cv2
 import os
 from datetime import datetime
 
-def generate_timestamp(video):
-    # Open the video file
-    cap = cv2.VideoCapture(video)
+# Open the video file
+cap = cv2.VideoCapture("uploads/step.mp4")
 
-    # Get the frames per second (FPS) of the video
-    fps = cap.get(cv2.CAP_PROP_FPS)
+# Get the frames per second (FPS) of the video and video properties
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = int(cap.get(cv2.CAP_PROP_FPS))
 
-    # Get the total number of frames in the video
-    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+output_folder = "videos"
+if not os.path.exists(output_folder):
+    os.mkdir(output_folder)
+output_path = os.path.join(output_folder, "output.mp4")
+video_writer = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
 
-    # Create a folder to save the timestamped frames
-    os.makedirs("video_timestamped", exist_ok=True)
+# Get the total number of frames in the video
+total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+frame_count = 0
 
-    # Loop through each frame of the video
-    frame_count = 0
-    while True:
-        # Read the next frame
-        ret, frame = cap.read()
-
-        # If there are no more frames, break out of the loop
-        if not ret:
-            break
-
-        # resize the frames
-        frame = cv2.resize(frame, (640, 480))
-
-        # Calculate the remaining time in seconds
-        time = (total_frames - frame_count) / fps
-
-        # convert time to datetime format
-        m, s = divmod(time, 60)
-        h, m = divmod(m, 60)
-        time = "{:0>2}:{:0>2}:{:05.2f}".format(int(h), int(m), s)
-
-        # Add the remaining time to the frame
-        cv2.putText(frame, f"{time}", (430, 450),
-                    cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (43, 165, 245), 2)
-
-        # Save the frame with timestamp to the folder
-        cv2.imwrite("./video_timestamped/frame_{frame_count}.jpg", frame)
-
-        # Increase the frame count
-        frame_count += 1
-
-    # Release the video capture object
-    cap.release()
+# putText variables
+text = 'Remaining Time: '
+font = cv2.FONT_HERSHEY_SIMPLEX
+org = (10, 70)
+fontScale = 3.0
+color = (0,165,255)
+thickness = 3
 
 
+# Loop through each frame of the video
+while True:
+    # Read the next frame
+    ret, frame = cap.read()
+
+    # If there are no more frames, break out of the loop
+    if not ret:
+        break
+
+    # Calculate the remaining time in seconds
+    time = (total_frames - frame_count) / fps
+
+    # convert time to datetime format
+    m, s = divmod(time, 60)
+    time = "{:0>2}:{:05.2f}".format(int(m), s)
+
+
+    
+    # Add the remaining time to the frame
+    cv2.putText(frame, text + f"{time}", org,
+                font, fontScale, color, thickness)
+
+
+    # Write the frame to the output video
+    video_writer.write(frame)
+
+
+    # Wait for a key press
+    if cv2.waitKey(int(1000/fps)) & 0xFF == ord('q'):
+        break
+
+    # Increase the frame count
+    frame_count += 1
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
